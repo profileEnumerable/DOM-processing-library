c19a0b83557f70affb451cc416254ac8aaed9869
profileEnumerable <profileenumerable@ukr.net>
2019-03-18 17:57:27 +0200

    fadeIn func was added

--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 const $ = function $(selector, context = document) {
 
-    const elements = Array.from(context.querySelectorAll(selector));
+    const elements = Array.from(context.querySelectorAll(selector)); //convert from NodeListOf<any> to array
 
     return {
         elements,
@@ -12,7 +12,7 @@ const $ = function $(selector, context = document) {
             return this;
         },
 
-        css(newCss) {
+        css(newCss) { //you need to pass an object in newCss
             this.elements.forEach(elem => {
                 Object.assign(elem.style, newCss);
             })
@@ -23,14 +23,29 @@ const $ = function $(selector, context = document) {
             this.elements.forEach(elem => {
                 elem.addEventListener(event, hendler, options);
             })
+            return this;
+        },
 
+        fadeIn(duration) {
+
+            var start = performance.now(); //save the start of execution
+
+            window.requestAnimationFrame(function continueShow(now) {
+
+                var process = now - start; //how much time has passed
+
+                if (process < duration) {
+                    elements.forEach(elem => {
+                        elem.style.opacity = process / duration //divide for get the range[0,1]
+                    })
+
+                    window.requestAnimationFrame(continueShow);
+                }
+            })
             return this;
         }
     }
 }
 
 
-$('div').html('<b>Member</b>');
-$('div').css({ color: 'red' });
-$('div').on('click',
-    function(event) { alert(event.type) }, { once: true })
+$('.block').fadeIn(2000);
